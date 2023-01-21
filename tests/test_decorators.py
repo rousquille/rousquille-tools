6a62e981c9f48eb3426bbb3890428948539314de
Fabien ROSSA <f.rossa@proximedia.be>
2023-01-21 21:02:47 +0100

    reformat with black

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -12,6 +12,6 @@ def test_time_def_decorator_output():
     def func1():
         time.sleep(5)
 
-    with patch('sys.stdout', new=StringIO()) as fake_stdout:
+    with patch("sys.stdout", new=StringIO()) as fake_stdout:
         func1()
         assert fake_stdout.getvalue() == expected_stdout
